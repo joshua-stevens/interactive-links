df5ca2f5fd9f1223469f4f26ac62543d26874c3a
joshua-stevens <joshan9na@gmail.com>
2020-02-19 11:20:10 -0500

    Create index.js

---
+++ js/index.js
@@ -0,0 +1,31 @@
+/* JS */
+
+
+
+
+
+	$("#saucer").click(function() {
+                $("#saucerLinks").slideToggle("slow");
+            });
+	$("#meddle").click(function() {
+                $("#meddleLinks").slideToggle("slow");
+            });
+	$("#animals").click(function() {
+                $("#animalsLinks").slideToggle("slow");
+            });
+	$("#wall").click(function() {
+                $("#wallLinks").slideToggle("slow");
+            });
+	$("#dark").click(function() {
+                $("#darkLinks").slideToggle("slow");
+            });
+
+
+
+
+
+
+
+
+
+
